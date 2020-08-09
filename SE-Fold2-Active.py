6b957b8bf109ffd3b51f5bba38144f676cd9c863
Arman Farhoudi <68311743+abc1399f@users.noreply.github.com>
2020-08-09 13:46:05 +0430

    Update SE-Fold2-Active.py

--- SE-Fold2-Active.py
+++ SE-Fold2-Active.py
@@ -784,15 +784,23 @@ def evaluate(model, predict_dataloader, batch_size, epoch_th, dataset_name):
             valid_label_ids = torch.masked_select(label_ids, predict_mask)
             all_preds.extend(valid_predicted.tolist())
             all_labels.extend(valid_label_ids.tolist())
+            count=0
             for i in range(len(value)):
                 sigma=0
-                for j in range(len(value[i])):
-                    print(value[i][j].item())
-                    if value[i][j].item() > 0:
-                      #print("miniiii",sigma)
-                      sigma= sigma+ (value[i][j].item() * math.log(value[i][j].item()))
-                confidence.append(sigma)
-                print("sigmaaaaaa",sigma)
+                if value[i][0].size() == torch.Size([3]):
+                  count=count+1
+                  print("3",count)
+                if value[i][0].size() == torch.Size([2]):
+                  count=count+1
+                  print("2",count)
+                else:
+                    for j in range(len(value[i])):
+                        print(value[i][j].item())
+                        if value[i][j].item() > 0:
+                          #print("miniiii",sigma)
+                          sigma= sigma+ (value[i][j].item() * math.log(value[i][j].item()))
+                    confidence.append(sigma)
+                    print("sigmaaaaaa",sigma)
             # print(len(valid_label_ids),len(valid_predicted),len(valid_label_ids)==len(valid_predicted))
             total += len(valid_label_ids)
             correct += valid_predicted.eq(valid_label_ids).sum().item()
